fde42730a87ce996db75bb5282ab021738d7a8e9
mviseu <mariaviseu@gmail.com>
2018-03-23 08:45:04 +0000

    Ranges  implementation of BottomUpMerge

--- Week3/BottomUpMergeSort.h
+++ Week3/BottomUpMergeSort.h
@@ -5,25 +5,49 @@
 #include <vector>
 #include <iostream>
 
+template <typename RandomIt>
+struct Range {
+	Range(RandomIt start, RandomIt finish) : beg(start), end(finish) {}
+	RandomIt beg = nullptr;
+	RandomIt end = nullptr;
+};
+
+template<typename RandomIt> 
+std::vector<Range<RandomIt>> CreateFirstRange(RandomIt beg, RandomIt end) {
+	std::vector<Range<RandomIt>> oneElementRanges;
+	auto it = beg;
+	std::generate_n(std::back_inserter(oneElementRanges), std::distance(beg, end), [&it] () {return Range(it, ++it);});
+	return oneElementRanges;
+}
+
+
+template<typename RandomIt>
+void MergeAdjacentRanges(typename std::vector<Range<RandomIt>>::const_iterator rangeIt) {
+	std::vector<typename RandomIt::value_type> out;
+	Merge(rangeIt->beg,rangeIt->end, (rangeIt + 1)->beg, (rangeIt + 1)->end, std::back_inserter(out));
+	std::copy(out.begin(), out.end(), rangeIt->beg);
+}
 
 template <typename RandomIt>
-void BottomUpMergeAuxiliary(RandomIt beg, RandomIt mid, RandomIt end) {
-	std::vector<typename RandomIt::value_type> out;
-	out.reserve(end - beg);
-	Merge(beg, mid, mid, end, std::back_inserter(out));
-	std::copy(out.cbegin(), out.cend(), beg);
+std::vector<Range<RandomIt>> MergeAdjacentRanges(const std::vector<Range<RandomIt>>& ranges) {
+	std::vector<Range<RandomIt>> rangesMerged;
+	for(auto rangeIt = ranges.cbegin(); rangeIt < ranges.cend(); rangeIt += 2) {
+		if(rangeIt != ranges.cend() - 1) {
+			MergeAdjacentRanges<RandomIt>(rangeIt);
+			rangesMerged.push_back(Range<RandomIt>(rangeIt->beg, (rangeIt+1)->end));
+		} else {
+			rangesMerged.push_back(Range<RandomIt>(rangeIt->beg, rangeIt->end));
+		}
+	}
+	return rangesMerged;
 }
 
 template <typename RandomIt>
 void BottomUpMergeSort(RandomIt beg, RandomIt end) {
-	auto size = std::distance(beg, end);
-	auto sz = 2;
-	while(sz <= size) {
-		for(auto it = beg; it < end; it += sz) {
-			auto correctedSz = std::min(static_cast<long>(sz), std::distance(it, end));
-			BottomUpMergeAuxiliary(it, it + (correctedSz + 1) / 2, it + correctedSz);
-		}
-		sz *= 2;
+	auto ranges = CreateFirstRange(beg, end);
+	while(ranges.size() > 1) {
+		ranges = MergeAdjacentRanges(ranges);
 	}
-	BottomUpMergeAuxiliary(beg, beg + sz / 2, end);
 }
+
+
--- Week3/MergeMain.cpp
+++ Week3/MergeMain.cpp
@@ -4,29 +4,49 @@
 #include <iterator>
 #include <iostream>
 
+void printVec(const std::vector<int>& v) {
+
+	auto maxDisplayed = 100;
+	for(const auto& elem : v) {
+		std::cout << elem << ' ';
+		if (--maxDisplayed == 0) {
+			break;
+		}
+	}
+	std::cout << std::endl;	
+}
+
 int main() {
+	/*
 	std::vector<int> vec = {6, 3, 8, 1, 7, 3, 9, 1, 42, 1000, 66, 33, 99, 98, 95, 1000};
 	std::vector<int> mergedVec;
 	Merge(vec.begin(), vec.begin() + 4, vec.begin() + 4, vec.end(), std::back_inserter(mergedVec));
-
+	*/
+	/*
 	std::cout << "MERGE" << std::endl;
 	for(const auto elem : mergedVec) {
 		std::cout << elem << std::endl;
 	}
+	*/
 
+	/*
 	MergeSort(vec.begin(), vec.end());
 	std::cout << "MERGESORT" << std::endl;
 	for(const auto elem : vec) {
 		std::cout << elem << std::endl;
-	}
+	}*/
 
 
-	std::vector<int> vecBottomUp = {0, 1, 2 , 3, 4, 15, 10, 7, 1};
+	std::vector<int> vecBottomUp = {};
+	const auto bigNumber = 100000000;
+	auto i = bigNumber;
+	vecBottomUp.reserve(bigNumber);
+	std::generate_n(std::back_inserter(vecBottomUp), bigNumber, [&i](){ return i--; });
+	std::cout << "before:" << std::endl;
+	printVec(vecBottomUp);
 	BottomUpMergeSort(vecBottomUp.begin(), vecBottomUp.end());
-	std::cout << "BOTTOM UP MERGE SORT" << std::endl;
-	for(const auto& elem : vecBottomUp) {
-		std::cout << elem << std::endl;
-	}
+	std::cout << "after:" << std::endl;
+	printVec(vecBottomUp);
 
 	return 0;
 }
