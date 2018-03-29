4e3a25a985d14289a0b484cf1cca85400f577c3b
mviseu <mariaviseu@gmail.com>
2018-03-29 21:37:23 +0100

    Render() function to draw points and lines

--- Week3/colinear_points/openGLMain.cpp
+++ Week3/colinear_points/openGLMain.cpp
@@ -1,5 +1,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -12,6 +13,54 @@ void error_callback(int error, const char* description) {
 }
 
 
+struct Point2 {
+	Point2(int xCoor, int yCoor) : x(xCoor), y(yCoor) {}
+	GLfloat x{0};
+	GLfloat y{0};
+};
+
+std::vector<Point2> points = {Point2(-1, -1), Point2(1, 1)};
+
+void Render()
+{     
+    //clear color and depth buffer 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();//load identity matrix
+    
+    //glTranslatef(0.0f,0.0f,0.0f);//move forward 4 units
+    
+    glBegin(GL_LINES); //starts drawing of points
+    glColor3f(0.0f,0.0f,1.0f); //blue color
+    glVertex3f(0.5f,0.5f,0.0f);//upper-right corner
+    glVertex3f(-0.5f,-0.5,0.0f);//lower-left corner
+
+    glColor3f(1.0f,0.0f,0.0f); //blue color
+    glVertex3f(-0.5f,0.5f,0.0f);//upper-right corner
+    glVertex3f(0.5f,-0.5,0.0f);//lower-left corner
+
+
+    glColor3f(0.0f,1.0f,0.0f); //blue color
+    glVertex3f(0.0f,0.5f,0.0f);//upper-right corner
+    glVertex3f(0.0f,-0.5,0.0f);//lower-left corner
+    glEnd();//end drawing of points
+
+    glPointSize(10.0f);//set point size to 10 pixels
+    glBegin(GL_POINTS);
+    glColor3f(0.0f,0.0f,1.0f); //blue color
+    glVertex3f(0.5f,0.5f,0.0f);//upper-right corner
+    glVertex3f(-0.5f,-0.5,0.0f);//lower-left corner
+
+    glColor3f(1.0f,0.0f,0.0f); //blue color
+    glVertex3f(-0.5f,0.5f,0.0f);//upper-right corner
+    glVertex3f(0.5f,-0.5,0.0f);//lower-left corner
+
+
+    glColor3f(0.0f,1.0f,0.0f); //blue color
+    glVertex3f(0.0f,0.5f,0.0f);//upper-right corner
+    glVertex3f(0.0f,-0.5,0.0f);//lower-left corner
+    glEnd();
+}
+
 int main() {
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit()) {
@@ -20,29 +69,21 @@ int main() {
 	}
 	std::cout << "Initialization succeeded" << std::endl;
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My Title", NULL, NULL);
 	if(!window)  {
 		std::cout << "Window initialization failed" << std::endl;
 	}
 	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+
 	while (!glfwWindowShouldClose(window)) {
-		int width, height;
-		//glfwGetFramebufferSize(window, &width, &height);
-		//glViewport(0, 0, width, height);
-		
-		glLineWidth(2.5); 
-		glColor3f(1.0, 0.0, 0.0);
-		glBegin(GL_LINES);
-		glVertex3f(0.0, 0.0, 0.0);
-		glVertex3f(15, 0, 0);
-		glEnd();
-	    // Keep running
-	    
+	    //display
+		Render();
         glfwSwapBuffers(window);
         glfwPollEvents();
 	}
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
