4a353e64e182ad14d62af6393968c02cf5ed74e6
Maria Viseu <mariaviseu@gmail.com>
2018-07-02 08:16:15 +0100

    Complete Insert on RedBlackTree
    * Ensure root colour is always black
    * Ensure moved smart pointers are not dereferenced

--- Week5/red_black_tree/RedBlackTree.h
+++ Week5/red_black_tree/RedBlackTree.h
@@ -8,7 +8,7 @@ enum struct Colour {red, black};
 template <typename Key, typename Value>
 struct Node {
 	Node(const std::pair<Key, Value>& val, std::unique_ptr<Node> lf, std::unique_ptr<Node> rt, Colour linkParent)
-	: keyVal(val), left(std::move(lf)), right(std::move(right)), colour(linkParent) {}
+	: keyVal(val), left(std::move(lf)), right(std::move(rt)), colour(linkParent) {}
 	std::pair<Key, Value> keyVal;
 	std::unique_ptr<Node> left;
 	std::unique_ptr<Node> right;
@@ -25,8 +25,8 @@ std::unique_ptr<Node<Key, Value>> LeftRotation(std::unique_ptr<Node<Key, Value>>
 	auto newTop = std::move(top->right);
 	top->right = std::move(newTop->left);
 	newTop->left = std::move(top);
-	newTop->colour = top->colour;
-	top->colour = Colour::red;
+	newTop->colour = newTop->left->colour;
+	newTop->left->colour = Colour::red;
 	return newTop;
 }
 
@@ -38,8 +38,8 @@ std::unique_ptr<Node<Key, Value>> RightRotation(std::unique_ptr<Node<Key, Value>
 	auto newTop = std::move(top->left);
 	top->left = std::move(newTop->right);
 	newTop->right = std::move(top);
-	newTop->colour = top->colour;
-	top->colour = Colour::red;
+	newTop->colour = newTop->right->colour;
+	newTop->right->colour = Colour::red;
 	return newTop;
 }
 
@@ -58,33 +58,47 @@ std::unique_ptr<Node<Key, Value>> ColourFlip(std::unique_ptr<Node<Key, Value>> t
 }
 
 template<typename Key, typename Value>
-bool IsKeyLess(const Node<Key, Value>& node, const std::pair<Key, Value>& val) {
-	return node.keyVal.first < val.first;
+bool IsTargetLess(const Node<Key, Value>& node, const std::pair<Key, Value>& val) {
+	return val.first < node.keyVal.first;
 }
 
 template<typename Key, typename Value>
-bool IsKeySame(const Node<Key, Value>& node, const std::pair<Key, Value>& val) {
+bool IsTargetSame(const Node<Key, Value>& node, const std::pair<Key, Value>& val) {
 	return node.keyVal.first == val.first;
 }
 
 template<typename Key, typename Value>
-bool AreChildrenPopulated(const Node<Key, Value>& node) {
-	return node.left != nullptr;
+bool IsLeftRed(const Node<Key, Value>& node) {
+	return node.left != nullptr && node.left->colour == Colour::red;
+}
+
+template<typename Key, typename Value>
+bool IsRightRed(const Node<Key, Value>& node) {
+	return node.right != nullptr && node.right->colour == Colour::red;
 }
 
 template<typename Key, typename Value>
 std::unique_ptr<Node<Key, Value>> InsertRec(std::unique_ptr<Node<Key, Value>> nodePtr, const std::pair<Key, Value>& val) {
-	assert(nodePtr != nullptr);
-	if(IsKeySame(*nodePtr, val)) {
-		return nodePtr;
-	} else {
-		if(AreBlacChildrenPopulated(*nodePtr)) {
-			return InsertBelow(std::move(nodePtr), val);
-
-		} else {
-			return InsertCurrent(std::move(nodePtr), val);
-		}
+	if(nodePtr == nullptr) {
+		return std::make_unique<Node<Key, Value>>(val, nullptr, nullptr, Colour::red);
 	}
+	if(IsTargetSame(*nodePtr, val)) {
+		return nodePtr;
+	}
+	if(IsTargetLess(*nodePtr, val)) {
+		nodePtr->left = InsertRec(std::move(nodePtr->left), val);
+	}
+	assert(nodePtr != nullptr);
+	if(IsRightRed(*nodePtr) && !IsLeftRed(*nodePtr)) {
+		nodePtr = LeftRotation(std::move(nodePtr));
+	}
+	if(IsLeftRed(*nodePtr) && IsLeftRed(*nodePtr->left)) {
+		nodePtr = RightRotation(std::move(nodePtr));
+	}
+	if(IsLeftRed(*nodePtr) && IsRightRed(*nodePtr)) {
+		nodePtr = ColourFlip(std::move(nodePtr));
+	}
+	return nodePtr;
 }
 
 } // namespace
@@ -94,14 +108,15 @@ class RedBlackTree {
 public:
 	void Insert(const std::pair<Key, Value>& val);
 private:
-	Node<Key, Value> m_root;
+	std::unique_ptr<Node<Key, Value>> m_root;
 };
 
 template <typename Key, typename Value>
 void RedBlackTree<Key, Value>::Insert(const std::pair<Key, Value>& val) {
 	if(m_root == nullptr) {
-		m_root = std::make_unique(Node(val, nullptr, nullptr, Colour::black));
+		m_root = std::make_unique<Node<Key, Value>>(val, nullptr, nullptr, Colour::black);
 	} else {
 		m_root = InsertRec(std::move(m_root), val);
+		m_root->colour = Colour::black;
 	}
 }
--- Week5/red_black_tree/main.cpp
+++ Week5/red_black_tree/main.cpp
@@ -1,5 +1,18 @@
 #include "RedBlackTree.h"
+#include <iostream>
+#include <string>
 
 int main() {
+	RedBlackTree<std::string, int> tree;
+	tree.Insert(std::make_pair(std::string("S"), 3));
+	tree.Insert(std::make_pair(std::string("E"), 3));
+	tree.Insert(std::make_pair(std::string("A"), 3));
+	tree.Insert(std::make_pair(std::string("R"), 3));
+	tree.Insert(std::make_pair(std::string("C"), 3));
+	tree.Insert(std::make_pair(std::string("H"), 3));
+	tree.Insert(std::make_pair(std::string("X"), 3));
+	tree.Insert(std::make_pair(std::string("M"), 3));
+	tree.Insert(std::make_pair(std::string("P"), 3));
+	tree.Insert(std::make_pair(std::string("L"), 3));
 	return 0;
 }
